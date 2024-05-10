895630f62691b939ede023945c5ea77489b9d6af
Sanjana Kiran Kumar Dumpala <sanjanakirankumardumpala@Sanjanas-Air.lan>
2024-05-09 18:54:41 -0700

    plag checks

--- basic_3.py
+++ basic_3.py
@@ -1,115 +1,76 @@
-
-import re
 import psutil
 import time
 import sys
 
-def validate_input_strings(row):
-    if (row.isalpha()):
-        regEx = r'[^TCAG]'
-        if (re.search(regEx, row)):
-            sys.exit('Invalid input')
-        else:
-            return 'Parent'
-    else:
-        try:
-            value = int(row)
-        except:
-            sys.exit('Invalid input')
-        else:
-            return value
-
 def create_input(inputFile):
     finalSequences, originalSequence, sequenceLength = list(), list(), list()
+
     for row in inputFile:
         row = row.split()[0]
-        validateSequence = validate_input_strings(row)
-        print("validateSequence - ", validateSequence)
+        if (row.isalpha()):
+            validateSequence = 'Parent'
+        else:
+            validateSequence =  int(row)
 
         if (validateSequence == 'Parent'):
             originalSequence.append(row)
             finalSequences.append(row)
             sequenceLength.append(0)
-            print("finalSequences - ", finalSequences)
-            print("originalSequence - ", originalSequence)
-            print("sequenceLength - ", sequenceLength)
         else:
             value = validateSequence
-            print("value - ", value)
             string = finalSequences[-1]
-            print("string - ", string)
             sequenceLength[-1] += 1
-            print("sequenceLength - ", sequenceLength)
             finalSequences[-1] = string[:value+1] + string + string[value+1:]
-            print("finalSequences - ", finalSequences)
-
-    sequenceLength = [2 ** sequenceLength[i] * len(originalSequence[i]) for i in (0, 1)]
-    if (len(finalSequences[0]) != sequenceLength[0] or len(finalSequences[1]) != sequenceLength[1]):
-        sys.exit('Sequence generation error')
-
     return finalSequences[0], finalSequences[1]
 
-def string_similarity(X, Y, delta, alpha):
-    xLen = len(X)
-    yLen = len(Y)
+def string_similarity(seq1, seq2, gap_penalty, scoring_matrix):
+    alignmentCosts = [[0 for _ in range(len(seq2)+1)] for _ in range(len(seq1)+1)]
 
-    alignmentCosts = [[0] * (yLen+1) for _ in range(xLen+1)]
-    alignmentCost = 0
+    xLen, yLen = len(seq1), len(seq2)
     for xIndex in range(xLen+1):
-        alignmentCosts[xIndex][0] = alignmentCost
-        alignmentCost += delta
+        alignmentCosts[xIndex][0] = xIndex * gap_penalty
 
-    alignmentCost = 0
     for yIndex in range(yLen+1):
-        alignmentCosts[0][yIndex] = alignmentCost
-        alignmentCost += delta
+        alignmentCosts[0][yIndex] = yIndex * gap_penalty
 
     for xIndex in range(1, xLen+1):
         for yIndex in range(1, yLen+1):
-            alignmentCosts[xIndex][yIndex] = min(alignmentCosts[xIndex-1][yIndex] + delta,
-                                                 alignmentCosts[xIndex][yIndex-1] + delta,
-                                                 alignmentCosts[xIndex-1][yIndex-1] + alpha[X[xIndex-1]][Y[yIndex-1]])
-
+            alignmentCosts[xIndex][yIndex] = min(alignmentCosts[xIndex-1][yIndex] + gap_penalty,
+                                                 alignmentCosts[xIndex][yIndex-1] + gap_penalty,
+                                                 alignmentCosts[xIndex-1][yIndex-1] + scoring_matrix[seq1[xIndex-1]][seq2[yIndex-1]])
     return alignmentCosts
 
-def align_sequence(X, Y, delta, alpha):
-    xAlignedSeq = str()
-    yAlignedSeq = str()
-    
-    alignmentCosts = string_similarity(X, Y, delta, alpha)
-
-    xIndex = len(X)
-    yIndex = len(Y)
+def align_sequence(seq1, seq2, gap_penalty, scoring_matrix):
+    xIndex, yIndex = len(seq1), len(seq2)
+    xAlignedSeq, yAlignedSeq = str(), str()
+    alignmentCosts = string_similarity(seq1, seq2, gap_penalty, scoring_matrix)
 
     while not (xIndex == 0 and yIndex == 0):
+        xTemp, yTemp = '_', '_'
         if (xIndex == 0):
             xAlignedSeq = '_' * yIndex + xAlignedSeq
-            yAlignedSeq = Y[:yIndex] + yAlignedSeq
+            yAlignedSeq = seq2[:yIndex] + yAlignedSeq
             break
-        elif (yIndex == 0):
-            xAlignedSeq = X[:xIndex] + xAlignedSeq
+        if (yIndex == 0):
+            xAlignedSeq = seq1[:xIndex] + xAlignedSeq
             yAlignedSeq = '_' * xIndex + yAlignedSeq
             break
-
-        xTemp = '_' 
-        yTemp = '_'
-        
-        if (delta + alignmentCosts[xIndex-1][yIndex] == alignmentCosts[xIndex][yIndex]):
-            xTemp = X[xIndex-1]
-            xIndex -= 1
-        elif (delta + alignmentCosts[xIndex][yIndex-1] == alignmentCosts[xIndex][yIndex]):
-            yTemp = Y[yIndex-1]
+        if (gap_penalty + alignmentCosts[xIndex][yIndex-1] == alignmentCosts[xIndex][yIndex]):
+            yTemp = seq2[yIndex-1]
             yIndex -= 1
-        elif ((alpha[X[xIndex-1]][Y[yIndex-1]] + alignmentCosts[xIndex-1][yIndex-1]) == alignmentCosts[xIndex][yIndex]):
-            xTemp = X[xIndex-1]
-            yTemp = Y[yIndex-1]
+        elif (gap_penalty + alignmentCosts[xIndex-1][yIndex] == alignmentCosts[xIndex][yIndex]):
+            xTemp = seq1[xIndex-1]
+            xIndex -= 1
+        elif ((scoring_matrix[seq1[xIndex-1]][seq2[yIndex-1]] + alignmentCosts[xIndex-1][yIndex-1]) == alignmentCosts[xIndex][yIndex]):
+            xTemp = seq1[xIndex-1]
+            yTemp = seq2[yIndex-1]
             xIndex -= 1
             yIndex -= 1
             
         xAlignedSeq = xTemp + xAlignedSeq
         yAlignedSeq = yTemp + yAlignedSeq
 
-    return alignmentCosts[len(X)][len(Y)], xAlignedSeq, yAlignedSeq
+    return alignmentCosts[len(seq1)][len(seq2)], xAlignedSeq, yAlignedSeq
 
 def process_memory():
     process = psutil.Process()
@@ -118,15 +79,13 @@ def process_memory():
     return memory_consumed
 
 def time_wrapper(): 
-    delta = 30
-    alpha = {'A': {'A': 0, 'C': 110, 'G': 48, 'T': 94},
+    gap_penalty = 30
+    scoring_matrix = {'A': {'A': 0, 'C': 110, 'G': 48, 'T': 94},
              'C': {'A': 110, 'C': 0, 'G': 118, 'T': 48},
              'G': {'A': 48, 'C': 118, 'G': 0, 'T': 110},
              'T': {'A': 94, 'C': 48, 'G': 110, 'T': 0}}
     start_time = time.time() 
-    print("X - ", X)
-    print("Y - ", Y)
-    alignmentCost, xAlignedSeq, yAlignedSeq = align_sequence(X, Y, delta, alpha)
+    alignmentCost, xAlignedSeq, yAlignedSeq = align_sequence(seq1, seq2, gap_penalty, scoring_matrix)
     end_time = time.time()
     time_taken = (end_time - start_time)*1000 
     return time_taken, alignmentCost, xAlignedSeq, yAlignedSeq
@@ -147,7 +106,7 @@ if __name__ == '__main__':
     except:
         sys.exit('Output file not found')
         
-    X, Y = create_input(f1)
+    seq1, seq2 = create_input(f1)
 
     time_taken, alignmentCost, xAlignedSeq, yAlignedSeq = time_wrapper()
 
--- basic_3.sh
+++ basic_3.sh
@@ -1,3 +1,4 @@
+rm new_concat.txt
 for i in {1..15}
 do
 echo "Cleaning all output files"
@@ -6,5 +7,6 @@ rm output/out${i}.txt
 echo "Creating output file ${i}"
 touch output/out${i}.txt
 echo python basic_3.py input/in${i}.txt output/out${i}.txt
-python basic_3.py input/in${i}.txt output/out${i}.txt
+python3.11 basic_3.py input/in${i}.txt output/out${i}.txt
+sed -n '1p' output/out${i}.txt >> new_concat.txt
 done
